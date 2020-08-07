28c38e74cf09f678afb5d79281663bb31f6fd030
Kim Phong <kimphong2910@Kims-Mac-mini.local>
2020-08-07 17:23:07 +0700

    modify left sidebar of page

--- FirstProjectwithCakePHP4/cgi-bin/templates/element/left-sidebar.php
+++ FirstProjectwithCakePHP4/cgi-bin/templates/element/left-sidebar.php
@@ -1,9 +1,14 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
+    <a href="<?= $this->Url->build('/admin', ["fullBase" => true]) ?>" class="brand-link">
+    <?=
+        $this->Html->image("/dist/img/AdminLTELogo.png", [
+            "alt" => "AdminLTE Logo",
+            "class" => "brand-image img-circle elevation-3",
+            "style" => "opacity: .8",
+        ]);
+    ?>
       <span class="brand-text font-weight-light">Academics Manage</span>
     </a>
 
@@ -12,7 +17,12 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <?=
+            $this->Html->image("/dist/img/user2-160x160.jpg", [
+                "class" => "img-circle elevation-2",
+                "alt" => "User Image",
+            ]);
+        ?>
         </div>
         <div class="info">
           <a href="#" class="d-block">Kim Phong</a>
@@ -24,7 +34,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- dashboards -->
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="<?= $this->Url->build('/admin', ["fullBase" => true]) ?>" class="nav-link">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                     Dashboards
@@ -43,13 +53,13 @@
                 </a>
                 <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="pages/charts/flot.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/add-college', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-plus nav-icon"></i>
                     <p>Add College</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/charts/inline.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/list-colleges', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-list-ul nav-icon"></i>
                     <p>List College</p>
                     </a>
@@ -68,13 +78,13 @@
                 </a>
                 <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="pages/charts/flot.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/add-branch', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-plus nav-icon"></i>
                     <p>Add Branch</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/charts/inline.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/list-branches', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-list-ul nav-icon"></i>
                     <p>List Branch</p>
                     </a>
@@ -93,13 +103,13 @@
                 </a>
                 <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="pages/charts/flot.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/add-student', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-plus nav-icon"></i>
                     <p>Add Student</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/charts/inline.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/list-students', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-list-ul nav-icon"></i>
                     <p>List Student</p>
                     </a>
@@ -118,13 +128,13 @@
                 </a>
                 <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="pages/charts/flot.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/add-staff', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-plus nav-icon"></i>
                     <p>Add Staff</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/charts/inline.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/list-staffs', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-list-ul nav-icon"></i>
                     <p>List Staff</p>
                     </a>
@@ -143,19 +153,19 @@
                 </a>
                 <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="pages/charts/flot.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/college-report', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-clipboard nav-icon"></i>
                     <p>College Report</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/charts/flot.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/student-report', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-clipboard nav-icon"></i>
                     <p>Student Report</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="pages/charts/inline.html" class="nav-link">
+                    <a href="<?= $this->Url->build('/admin/staff-report', ["fullBase" => true]) ?>" class="nav-link">
                     <i class="fas fa-clipboard nav-icon"></i>
                     <p>Staff Report</p>
                     </a>
