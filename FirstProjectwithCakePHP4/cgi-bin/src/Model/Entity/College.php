00ec0f839cf3d346f3fdbb82c19aa9f0c4b5ff6a
Kim Phong <kimphong2910@Kims-Mac-mini.local>
2020-08-13 17:02:18 +0700

    modify access entity

--- FirstProjectwithCakePHP4/cgi-bin/src/Model/Entity/College.php
+++ FirstProjectwithCakePHP4/cgi-bin/src/Model/Entity/College.php
@@ -3,6 +3,16 @@ namespace App\Model\Entity;
 use Cake\ORM\Entity;
 
 class College extends Entity{
-    protected $_accessible = [];
+    protected $_accessible = [
+        "name" => true,
+        "short_name" => true,
+        "description" => true,
+        "location" => true,
+        "total_faculty" => true,
+        "contact_number" => true,
+        "email" => true,
+        "cover_image" => true,
+        "status" => true, 
+    ];
 }
 ?>
