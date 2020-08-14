23988631f2b3e7f70971a381ab091346a064512d
Kim Phong <kimphong2910@Kims-Mac-mini.local>
2020-08-14 11:42:58 +0700

    set table connect in database and create belongsto association with college table

--- FirstProjectwithCakePHP4/cgi-bin/src/Model/Table/BranchesTable.php
+++ FirstProjectwithCakePHP4/cgi-bin/src/Model/Table/BranchesTable.php
@@ -5,7 +5,10 @@ use Cake\ORM\Table;
 class BranchesTable extends Table{
     public function initialize(array $config): void
     {
-        
+        $this->setTable('tbl_branches');
+        $this->belongsTo("branch_college", [
+            "className" => "Colleges"
+        ])->setForeignKey("college_id");
     }
 }
 ?>
