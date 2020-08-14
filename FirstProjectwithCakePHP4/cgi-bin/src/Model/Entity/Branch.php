ab87614649824e24d43fab66dd8cff150823bf51
Kim Phong <kimphong2910@Kims-Mac-mini.local>
2020-08-14 11:41:01 +0700

    modify access in branch entity

--- FirstProjectwithCakePHP4/cgi-bin/src/Model/Entity/Branch.php
+++ FirstProjectwithCakePHP4/cgi-bin/src/Model/Entity/Branch.php
@@ -3,6 +3,16 @@ namespace App\Model\Entity;
 use Cake\ORM\Entity;
 
 class Branch extends Entity{
-    protected $_accessible = [];
+    protected $_accessible = [
+        "name" => true,
+        "description" => true,
+        "college_id" => true,
+        "start_date" => true,
+        "end_date" => true,
+        "total_seats" => true,
+        "total_durations" => true,
+        "status" => true,
+        "created_at" => true,
+    ];
 }
 ?>
