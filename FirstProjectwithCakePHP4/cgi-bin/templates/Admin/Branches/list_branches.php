a74363fbccc3d69ad98ac3654d83ded1243a2d94
Kim Phong <kimphong2910@Kims-Mac-mini.local>
2020-08-14 11:45:34 +0700

    modify list branches view for controller

--- FirstProjectwithCakePHP4/cgi-bin/templates/Admin/Branches/list_branches.php
+++ FirstProjectwithCakePHP4/cgi-bin/templates/Admin/Branches/list_branches.php
@@ -10,66 +10,87 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
     <div class="container-fluid">
-    <div class="row mb-2">
-        <div class="col-sm-6">
-        <h3>List Branches</h3 >
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h3>List Branches</h3 >
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">List Branches</li>
+                </ol>
+            </div>
         </div>
-        <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">List Branches</li>
-        </ol>
-        </div>
-    </div>
     </div><!-- /.container-fluid -->
 </section>
 
 <!-- Main content -->
 <section class="content"> 
     <div class="container-fluid">
-    <div class="row">
-        <!-- left column -->
-        <div class="col-md-12">  
-        <!-- general form elements disabled -->
-        <div class="card card-secondary">
-            <div class="card-header">
-            <h3 class="card-title">List Branches</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <table id="tbl-branches" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                    <th>#ID</th>
-                    <th>Branch Info</th>
-                    <th>College Name</th>
-                    <th>Total Seats</th>
-                    <th>Total Durations</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                
-                </tbody>
-                <tfoot>
-                <tr>
-                    <th>#ID</th>
-                    <th>Branch Info</th>
-                    <th>College Name</th>
-                    <th>Total Seats</th>
-                    <th>Total Durations</th>
-                    <th>Action</th>
-                </tr>
-                </tfoot>
-              </table>
-            </div>
-        </div>
-        <!-- /.card -->
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">  
+                <!-- general form elements disabled -->
+                <div class="card card-secondary">
+                    <div class="card-header">
+                        <h3 class="card-title">List Branches</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="tbl-branches" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>Branch Info</th>
+                                    <th>College Name</th>
+                                    <th>Total Seats</th>
+                                    <th>Total Durations</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    if (count($branches)>0) {
+                                        foreach ($branches as $index => $branch) {
 
+                                ?>
+                                    <tr>
+                                        <th><?=$branch->id?></th>
+                                        <th><?= "<b>Name: </b>".$branch->name."<br><b>Session Start Date: </b>".$branch->start_date."<br><b>Session End Date: </b>".$branch->end_date?></th>
+                                        <th><?= isset($branch->branch_college->name) ? $branch->branch_college->name : "N/A"?></th>
+                                        <th><?= $branch->total_seats?></th>
+                                        <th><?= $branch->total_durations?></th>
+                                        <th>
+                                        <form id="frm-delete-branch-<?= $branch->id?>" action="<?= $this->Url->build('/admin/delete-branch/'.$branch->id)?>" method="post">
+                                            <input type="hidden" name="id" value="<?= $branch->id?>"/>
+                                        </form>
+                                        <a class="btn btn-warning" href="<?= $this->Url->build('/admin/edit-branch/'.$branch->id, ['fullBase' => true])?>"><i class="fa fa-pencil-alt"></i></a>
+                                         <a class="btn btn-danger" href="javascript:void(0)" onclick="if(confirm('Are you sure want to delete?')){ $('#frm-delete-branch-<?= $branch->id?>').submit() }"><i class="fa fa-trash-alt"></i></a>
+                                        </th>
+                                    </tr>
+                                <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>#ID</th>
+                                    <th>Branch Info</th>
+                                    <th>College Name</th>
+                                    <th>Total Seats</th>
+                                    <th>Total Durations</th>
+                                    <th>Action</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.card -->
+            </div>
+            <!--/.col (left) -->
         </div>
-        <!--/.col (left) -->
-    </div>
-    <!-- /.row -->
+        <!-- /.row -->
     </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
